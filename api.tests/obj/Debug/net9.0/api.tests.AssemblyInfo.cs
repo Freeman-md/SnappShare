4b9e9551ff555511bf40068421df82ae83928c59
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-04-12 05:40:09 +0100

    feat(dependencies): add Azure.Messaging.ServiceBus and Azure.Core.Amqp packages

--- api.tests/obj/Debug/net9.0/api.tests.AssemblyInfo.cs
+++ api.tests/obj/Debug/net9.0/api.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89667eb35ad037f4baef564d62f44839241476d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0002faf08b8ffa258fd042b7930607e017e9a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
